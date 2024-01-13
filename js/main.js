b98bb5e32b1ae8d89f022cb7c9b75fc84bded8c2
AbdullazadeFarid <abdullazadefarid@mail.ru>
2024-01-13 21:47:01 +0300

    ..

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function submit(e) {
     }, 2000);
   }
   function display_error() {
-    form.reset();
+    // form.reset();
     form.style.display = "none";
     error_message.style.display = "flex";
 
@@ -72,3 +72,124 @@ error_icon.src = "./assets/781548.png";
 var erron_text = document.createElement("h3");
 error_message.appendChild(erron_text);
 erron_text.textContent = "XeTA";
+
+
+
+var firstname = document.querySelector("#firstname")
+var firsname_error = document.querySelector(".firsname_error")
+
+var lastname = document.querySelector("#lastname")
+lastname_error = document.querySelector(".lastname_error")
+
+var fathername = document.querySelector("#fathername")
+var fathername_error = document.querySelector(".fathername_error")
+
+
+var mail = document.querySelector("#mail")
+var mail_error = document.querySelector(".mail_error")
+
+var number = document.querySelector("#number")
+var number_error = document.querySelector(".number_error")
+
+var select = document.querySelector("#select")
+var select_error_top = document.querySelector(".select_error_top")
+
+var select_bottom = document.querySelector(".select_bottom")
+var select_error_bottom= document.querySelector(".select_error_bottom")
+
+var textarea = document.querySelector(".textarea")
+var textarea_error = document.querySelector(".textarea_error")
+form.addEventListener("submit", function(e){
+    e.preventDefault()
+var a =true
+    if(a=true){
+
+        if (lastname.value.trim()===""){
+            lastname_error.textContent="Bos buraxma"
+
+        }
+        else{
+            lastname_error.textContent=""
+             a = false
+        }
+
+
+        if (firstname.value.trim()===""){
+            firsname_error.textContent="Bos buraxma"
+            a=false
+        }
+        else{
+            firsname_error.textContent=""
+
+        }
+
+        if (fathername.value.trim()===""){
+            fathername_error.textContent="Bos buraxma"
+        }
+        else{
+            fathername_error.textContent=""
+            a=false
+        }
+
+
+        if (!mail.value.includes("@")){
+            mail_error.textContent="@ isaresinen istifade edin ,    Meselen:asasas@.ru "
+        }
+        else{
+            mail_error.textContent=""
+            a=false
+
+        }
+
+
+        if (number.value.trim()===""){
+            number_error.textContent="Bos buraxma"
+        }
+        else{
+            number_error.textContent=""
+            a=false
+        }
+
+
+
+
+        if (select.value==="Secin"){
+            select_error_top.textContent="Zehmet olmasa asagidaki melumatlardan birini secin"
+        }
+        else{
+            select_error_top.textContent=""
+            a=false
+        }
+
+        if ( select_bottom.value==="Secin"){
+            select_error_bottom.textContent="Zehmet olmasa asagidaki melumatlardan birini secin"
+        }
+        else{
+            select_error_bottom.textContent=""
+            a=false
+        }
+
+
+        if (textarea.value===""){
+            textarea_error.textContent="Zehmet olmasa sikayetinizi bildirin"
+        }
+        else{
+            textarea_error.textContent=""
+            a=false
+
+        }
+    }
+    //  if (a=false){
+    //     form.addEventListener("submit", submit);
+
+    // }
+
+
+
+
+
+
+
+});
+
+
